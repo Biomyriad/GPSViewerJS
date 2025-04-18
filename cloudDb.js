80fa78a9fe044417df15e5e072dc35bf9c877c6f
Biomyriad <biomyriad@gmail.com>
2025-04-18 14:27:10 -0700

    UpDaTe

--- cloudDb.js
+++ cloudDb.js
@@ -22,19 +22,19 @@ cloudDb.newRecObj = (keyValObj) => {
   var recObj = {fields: {}}
   let keys = Object.keys(keyValObj)
   
-   keys.forEach((item) => {
-     console.log(item + ' ' + keyValObj[item])
-     recObj.fields[item] = keyValObj[item]
-   })
-   console.log(recObj)
+  keys.forEach((item) => {
+    console.log(item + ' ' + keyValObj[item])
+    recObj.fields[item] = keyValObj[item]
+  })
+  console.log(recObj)
   
 }
 
-cloudDb.getAll = async (offsetId='') => {
+cloudDb.getSchema = async () => {
   let retRecs = []
   try {
 
-    let response = await fetch(`${cloudDb.base_host}/${cloudDb.base_id}/incident%20reports%20and%20observations?pageSize=${offsetId}`, {
+    let response = await fetch(`${cloudDb.base_host}/meta/bases/${cloudDb.base_id}/tables`, {
 
       method: 'GET',
       headers: {
@@ -42,18 +42,11 @@ cloudDb.getAll = async (offsetId='') => {
         'Authorization': `Bearer ${cloudDb.token()}`
       }
     });
-    let data = await response.json();
-    
-    retRecs.push(...data.records)
-    if(data.offset) retRecs.push(...await cloudDb.getAll('&offset='+data.offset))
 
-    //var currentUpdate = null
-    //data.records.forEach(rec => {
-    //  if (rec.fields.current) {
-    //    currentUpdate = rec.fields
-    //  }
-    //});
-    return retRecs
+    let data = await response.json();
+    //console.log(data)
+
+    return data
   } catch (err) {
     console.log('AT error: ' + err)
     return false
@@ -76,7 +69,7 @@ cloudDb.getIncidentByDateRange= async (startTimeStamp, endTimeStamp) => {
   let atSort = `&sort[0][field]=Date+and+Time+of+Incident&sort[0][direction]=asc`
 
   let atEncodedParams = encodeURI(atFilter+atSort)
-  console.log(atFilter+atSort)
+  //console.log(atFilter+atSort)
   //return
 
   try {
@@ -105,8 +98,38 @@ cloudDb.getIncidentByDateRange= async (startTimeStamp, endTimeStamp) => {
   }
 }
 
-cloudDb.getOne = async (recId) => {
-  let response = await fetch(`${cloudDb.base_host}/${cloudDb.base_id}/`+recId, {
+cloudDb.getAll = async (tableName, offsetId='') => {
+  let retRecs = []
+  try {
+
+    let response = await fetch(`${cloudDb.base_host}/${cloudDb.base_id}/${tableName}?pageSize=${offsetId}`, {
+
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${cloudDb.token()}`
+      }
+    });
+    let data = await response.json();
+    
+    retRecs.push(...data.records)
+    if(data.offset) retRecs.push(...await cloudDb.getAll(tableName, '&offset='+data.offset))
+
+    //var currentUpdate = null
+    //data.records.forEach(rec => {
+    //  if (rec.fields.current) {
+    //    currentUpdate = rec.fields
+    //  }
+    //});
+    return retRecs
+  } catch (err) {
+    console.log('AT error: ' + err)
+    return false
+  }
+}
+
+cloudDb.getOne = async (recId, tableName) => {
+  let response = await fetch(`${cloudDb.base_host}/${cloudDb.base_id}/${tableName}/`+recId, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -117,7 +140,7 @@ cloudDb.getOne = async (recId) => {
   return data
 }
 
-cloudDb.new = async (recObj) => {
+cloudDb.new = async (recObj,tableName) => {
   //var reqData = {
   //  records: [
   //    {
@@ -128,7 +151,7 @@ cloudDb.new = async (recObj) => {
   //    }
   //  ]
   //}
-  let response = await fetch(`${cloudDb.base_host}/${cloudDb.base_id}/Updates`, {
+  let response = await fetch(`${cloudDb.base_host}/${cloudDb.base_id}/${tableName}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -140,7 +163,7 @@ cloudDb.new = async (recObj) => {
   return data
 }
 
-cloudDb.update = async (recId,recObj) => {
+cloudDb.update = async (recId,recObj,tableName) => {
   //var reqData = {
   //  records: [
   //    {
@@ -151,7 +174,7 @@ cloudDb.update = async (recId,recObj) => {
   //    }
   //  ]
   //}
-  let response = await fetch(`${cloudDb.base_host}/${cloudDb.base_id}/Updates/`+recId, {
+  let response = await fetch(`${cloudDb.base_host}/${cloudDb.base_id}/${tableName}/`+recId, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
@@ -163,8 +186,8 @@ cloudDb.update = async (recId,recObj) => {
   return data
 }
 
-cloudDb.delete = async (recId) => {
-  let response = await fetch(`${cloudDb.base_host}/${cloudDb.base_id}/Updates/`+recId, {
+cloudDb.delete = async (recId,tableName) => {
+  let response = await fetch(`${cloudDb.base_host}/${cloudDb.base_id}/${tableName}/`+recId, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
--- reports.js
+++ reports.js
@@ -8,66 +8,220 @@
 // "Picture or other attachment if needed"
 // "Incident Report Picture"
 
+const tblProperty = "All Properties"
+const tblIncident = "Incident Reports and Observations"
+
+var propertiesList = null
+var incidentReportingOfficer = null
+var loadedReportList = null
+
+async function initAirTable() {
+
+}
+
+async function preLoad() {
+  // Incident Reports and Observations reporting off list options
+  // {"id": "sel9P7sw8sICjlSrR","name": "Amber Schumaker ","color": "blueLight2"}
+  //tables[4].fields[3].options.choices
+
+  var baseSchema = await cloudDb.getSchema()
+  incidentReportingOfficer = baseSchema.tables[4].fields[3].options.choices
+
+  var props = await cloudDb.getAll(tblProperty)
+  propertiesList = props
+  //console.log(props)
+
+  console.log(lookupProperty("reczX6h9NAVWSF9f5"))
+}
+
+function lookupProperty(propId) {
+  return propertiesList.find(rec => rec.id == propId).fields.Name
+}
+
 async function tst() {
   let startTimeStamp = new Date("4/10/2025")
   let endTimeStamp = new Date("4/11/2025")
 
-  let listOfSaves = await cloudDb.getIncidentByDateRange(startTimeStamp, endTimeStamp)
+  loadedReportList = await cloudDb.getIncidentByDateRange(startTimeStamp, endTimeStamp)
 
-  console.log(listOfSaves.length)
-  for (i in listOfSaves) {
+  console.log(loadedReportList)
+  document.getElementById("recordslist").replaceChildren()
+  createReportHtml(loadedReportList[4])
+  return
 
-// console.log(listOfSaves[i].fields['Record Code'])
-// console.log(listOfSaves[i].fields['Property Code'])
-//console.log(new Date(listOfSaves[i].fields['Date and Time of Incident']))
-// console.log(listOfSaves[i].fields['Reporting Officer'])
-// console.log(listOfSaves[i].fields['Description of incident or observation'])
-
-let d = new Date(listOfSaves[i].fields['Date and Time of Incident'])
-const dateTimeLocalValue = (new Date(d.getTime() - d.getTimezoneOffset() * 60000).toISOString()).slice(0, -1);
-
-
-let imgUrl = ''
-let imgH = "100"
-let img = listOfSaves[i].fields['Picture or other attachment if needed']
-if(img != undefined) {
-  console.log(img[0].thumbnails.large.url)
-  imgUrl = img[0].thumbnails.large.url
-  imgH = img[0].thumbnails.large.height/3
-}
-
-
-
-let item = `<details >
-<summary>${new Date(listOfSaves[i].fields['Date and Time of Incident']).toLocaleTimeString()} ${listOfSaves[i].fields['Record Code']}</summary>
-<article style="background-color: rgb(38, 47, 66);">
-  <article name="Property Code">${listOfSaves[i].fields['Record Code']}</article>
-
-    <input name="Date and Time of Incident" type="datetime-local" id="dateselect" value="${dateTimeLocalValue}"> 
-    
-    <select name="Reporting Officer" id="routeselect">
-      <option selected disabled value="">${listOfSaves[i].fields['Reporting Officer']}</option>
-    </select>
-
-    <textarea name="Description of incident or observation">${listOfSaves[i].fields['Description of incident or observation']}</textarea>
-
-    <article name="Picture or other attachment if needed"><img src="${imgUrl}" style="height:${imgH}px;"></article>
-
-        
-</article>
-</details>`
-
-let reportList = document.getElementById('recordslist')
-reportList.innerHTML = reportList.innerHTML + item
-
-
-
-
-
-  }
-  console.log(listOfSaves.length)
+  console.log(loadedReportList.length)
   // let rec = await cloudDb.getOne("recHu52FwctlfyJRL")
   // console.log(rec)
 }
 
 //IS_AFTER({Date and Time of Incident}, "2025-04-09")
+
+
+function createReportHtml(rec) {
+
+//<details id="recQSf7OEq5OO436H-record">
+// 	<summary id="recQSf7OEq5OO436H-title">11:50:00 PM 173 - Evans Creek @ Woodbridge</summary>
+// 	<article id="recQSf7OEq5OO436H-content class="rec-content">
+// 		<select id="recQSf7OEq5OO436H-propertycode">
+// 			<option value="reczX6h9NAVWSF9f5">176 - AMC Redmond Place</option>
+// 		</select>
+// 		<input id="recQSf7OEq5OO436H-datetime" type="datetime-local" value="2025-04-10T23:50:00.000">
+// 		<select id="recQSf7OEq5OO436H-officerselect">
+// 			<option value="sel9P7sw8sICjlSrR">Amber Schumaker </option>
+// 		</select>
+// 		<textarea id="recQSf7OEq5OO436H-description"></textarea>
+// 		<article id="recQSf7OEq5OO436H-pictures" style="display: flex; justify-content: space-between;">
+// 			<img src="" style="height: 100px; border-radius: 4px">
+// 		</article>
+// 	</article>
+//</details>
+
+
+// -record
+// -title
+// -content
+// -propertycode
+// -datetime
+// -dateselect
+// -officerselect
+// -description
+// -pictures
+
+  var mainCont = document.createElement("details");
+  mainCont.setAttribute("id",rec.id+"-record");
+
+	var title = document.createElement("summary");
+  title.setAttribute("id",rec.id+"-title");
+  title.innerHTML = new Date(rec.fields['Date and Time of Incident']).toLocaleTimeString() + " " + rec.fields['Record Code']
+
+	var content = document.createElement("article");
+  content.setAttribute("id",rec.id+"-content");
+  content.setAttribute("class","rec-content");
+
+	var prop = document.createElement("select"); // Property Code
+	prop.setAttribute("id",rec.id+"-propertycode");
+  prop.onchange = recOnChange
+
+  propertiesList.forEach(item => {
+  var propOpt = document.createElement("option"); //
+    if(rec.fields["Property Code"][0] == item.id) propOpt.setAttribute("selected","");
+    //propOpt.setAttribute("disabled","");
+    propOpt.setAttribute("value",item.id);
+    propOpt.innerText = item.fields.Name
+    prop.appendChild(propOpt)
+  });
+
+  let d = new Date(rec.fields['Date and Time of Incident'])
+	var dateTime = document.createElement("input"); //  Date and Time of Incident
+	dateTime.setAttribute("id",rec.id+"-datetime");
+	dateTime.setAttribute("type","datetime-local");
+	dateTime.setAttribute("value",(new Date(d.getTime() - d.getTimezoneOffset() * 60000).toISOString()).slice(0, -1));
+  dateTime.onchange = recOnChange
+
+	var officer = document.createElement("select"); // Reporting Officer
+	officer.setAttribute("id",rec.id+"-officerselect");
+  officer.onchange = recOnChange
+
+  incidentReportingOfficer.forEach(item => {
+    var officerOpt = document.createElement("option"); // 
+    if(rec.fields["Reporting Officer"] == item.name) officerOpt.setAttribute("selected","");
+    //officerOpt.setAttribute("disabled","");
+    officerOpt.setAttribute("value",item.id);
+    officerOpt.innerText = item.name
+    officer.appendChild(officerOpt)
+  })
+
+	var discr = document.createElement("textarea"); // Description of incident or observation
+  discr.setAttribute("id",rec.id+"-description")
+  discr.value = rec.fields['Description of incident or observation']
+  discr.onchange = recOnChange
+
+	var pic = document.createElement("article"); // Picture or other attachment if needed
+  pic.setAttribute("id",rec.id+"-pictures");
+  pic.setAttribute("style","display: flex; justify-content: space-between;");
+
+  rec.fields['Picture or other attachment if needed'].forEach(item => {
+    var pics = document.createElement("img");
+    pics.setAttribute("src",item.thumbnails.large.url);
+    pics.setAttribute("style","height: 100px; border-radius: 4px");
+    pic.appendChild(pics)
+  })
+
+  var _button = document.createElement("button");
+  _button.innerHTML = 'click me';
+  _button.onclick = async function()
+  {
+  console.log(rec.id + "<-----")
+  let re = await cloudDb.getOne(rec.id,tblIncident)
+  console.log(re)
+      //alert("hello, world");
+  }
+
+  mainCont.appendChild(title)
+  mainCont.appendChild(content)
+
+  content.appendChild(prop)
+  content.appendChild(dateTime)
+  content.appendChild(officer)
+  content.appendChild(discr)
+  content.appendChild(pic)
+  content.appendChild(_button)
+
+document.getElementById("recordslist").appendChild(mainCont)
+
+}
+
+function recOnChange(e) {
+  
+    //console.log(this.id); // logs the className of my_element
+    //console.log(e.target.id);
+
+    let rec = loadedReportList.find(rec => rec.id == this.id.substring(0,this.id.indexOf("-")))
+    console.log(rec)
+
+// -record
+// -title
+// -propertycode
+// -datetime
+// -dateselect
+// -officerselect
+// -description
+// -pictures
+
+    var record = document.getElementById(rec.id + "-record")
+    var title = document.getElementById(rec.id + "-title")
+    var content = document.getElementById(rec.id + "-content")
+    var propertyCode = document.getElementById(rec.id + "-propertycode")
+    var datetime = document.getElementById(rec.id + "-datetime")
+    var officerSelect = document.getElementById(rec.id + "-officerselect")
+    var description = document.getElementById(rec.id + "-description")
+    var pictures = document.getElementById(rec.id + "-pictures")
+
+
+    console.log("-----------------------------------------"+"record")
+    //console.log(title)
+    console.log("propertyCode: " + propertyCode.value + " || " + rec.fields["Property Code"][0])
+    console.log(propertyCode.value == rec.fields["Property Code"][0])
+
+    let d = new Date(rec.fields['Date and Time of Incident'])
+    console.log("datetime: " + datetime.value + " || " + ((new Date(d.getTime() - d.getTimezoneOffset() * 60000).toISOString()).slice(0, -1)).substring(0,16))
+    console.log(datetime.value == ((new Date(d.getTime() - d.getTimezoneOffset() * 60000).toISOString()).slice(0, -1)).substring(0,16))
+
+    console.log("officerSelect: " + officerSelect.options[officerSelect.selectedIndex].text + " || " + rec.fields["Reporting Officer"])
+    console.log(officerSelect.options[officerSelect.selectedIndex].text == rec.fields["Reporting Officer"])
+
+    console.log("description: " + description.value)
+    console.log(". . . . . . . .")
+    console.log(rec.fields['Description of incident or observation'])
+    console.log(description.value == rec.fields['Description of incident or observation'])
+    //console.log(pictures)
+    console.log("---------------------------")
+
+    title.classList.toggle("rec-content-edited")
+
+
+}
+
+function recordBtn() {
+  
+}
