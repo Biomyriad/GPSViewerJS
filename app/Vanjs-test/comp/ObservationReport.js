5419b39e2f8a6c794bf6125323cea7e0d3cdeda9
Biomyriad <biomyriad@gmail.com>
2025-12-26 22:39:22 -0800

    ...

--- app/Vanjs-test/comp/ObservationReport.js
+++ app/Vanjs-test/comp/ObservationReport.js
@@ -1,27 +1,49 @@
-const { div, button, input,p,hr,span,select,option,article,summary} = van.tags;
+const { div, button, input,p,hr,span,select,option,article,summary,details} = van.tags;
 
-export default function Reports() {
+export default function Reports(rec) {
 
-  return div({class: "container", style: "margin-top: 50px;"},
-    div({style: "display: flex; justify-content: space-around; align-items: center; margin-bottom: 40px;"},
-        button({onclick: dateNav},"<-"),
-        input({id:"shift-date", type:"date", style:"width: 150px; margin: 0;"}),
-        button({onclick: dateNav2},"->"),
-    ),
+  var routeColor = ""
+  var prop = dataBase.allProps.find(prec => prec.id == rec.fields['Property Code'][0])
+  if(prop.fields.Route.includes('South Route')) {
+    routeColor = "green"
+  }
+  if(prop.fields.Route.includes('NE Route')) {
+    if(routeColor == "green") {routeColor = "purple"}
+    else {routeColor = "blue"}
+  }
 
-    hr(
-        span("Filter by route"),
-        select({id:"filterbyroute"},
-            option({value:"none"},"No Filter"),
-            option({value:"South Route"},"South Route"),
-            option({value:"NE Route"},"NE Route"),
-        )
-    ),
-
-    article({id:"recordslist"},
-        summary({"aria-busy":"true"})
-    ),
-
-    div({id: "out"})
+return div(
+details({name: "incidentrecord", class: "incidentrecord", id: rec.id+"-record"},
+  div({},
+    
   )
+)
+)
 };
+
+///////////////////////
+// export default function Reports() {
+
+//   return div({class: "container", style: "margin-top: 50px;"},
+//     div({style: "display: flex; justify-content: space-around; align-items: center; margin-bottom: 40px;"},
+//         button({onclick: dateNav},"<-"),
+//         input({id:"shift-date", type:"date", style:"width: 150px; margin: 0;"}),
+//         button({onclick: dateNav2},"->"),
+//     ),
+
+//     hr(
+//         span("Filter by route"),
+//         select({id:"filterbyroute"},
+//             option({value:"none"},"No Filter"),
+//             option({value:"South Route"},"South Route"),
+//             option({value:"NE Route"},"NE Route"),
+//         )
+//     ),
+
+//     article({id:"recordslist"},
+//         summary({"aria-busy":"true"})
+//     ),
+
+//     div({id: "out"})
+//   )
+// };
