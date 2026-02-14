16cd5d64fbd20150103f1183807913e160ee4f1c
Biomyriad <biomyriad@gmail.com>
2026-02-13 18:11:42 -0800

    Oh,yah!

--- app/Vanjs-test/FastFields/Tst2.js
+++ app/Vanjs-test/FastFields/Tst2.js
@@ -3,6 +3,94 @@
 
 
 
+async function dipatchForm(){
+
+var data = 
+{
+    // NOTES: Looks like there is "Status (Is List of some kind)", "Priority (3: High, Medium, low, N/A)", "Project (String)", "Address", "Task Info"
+    "formId": 585136,
+    "name": "AOG REPORTING",
+    //"dueDate": "2017-12-03T08:38:27.771-08:00",//(optional,stringISO)
+    //"referenceId": "", //(optional,string) 	An external identiﬁer assigned to the dispatch which can be used to uniquely identify the Dispatch record in FastField. Often the reference Id is used to reference an existing identiﬁer from another system.
+    "autoCreateUsers": false,
+    "recipients": [
+        // {
+        //     "firstName": "Joe", //(autoCreateUsers only?)
+        //     "lastName": "Test", //(autoCreateUsers only?)
+        //     "email": "testuser@mergemobile.com" //(autoCreateUsers only?)
+        // },
+        {"userId": 333461}, // (OR UserId))
+    ],
+    "dispatchEmailSubject": "New Test Dispatched Form!",
+    "dispatchEmailMessage": "Please work ;P",
+    "message": "We can get chatty here and... maybe add html? This prob gets sanitized. You know what?! Lets try... <b>bold</b> <i>italics</i> <u>underline</u> <a style='border: 1px solid blue;' href='https://www.fastfieldforms.com'>link</a>",
+    "mergeFields": [ //(optional)
+      { 
+        "fieldKey": "My_Name",
+        "value": "This came from code!!"
+      }, 
+      // { 
+      //   "fieldKey": "name",
+      //   "value": "abc name"
+      // }, 
+      // {
+      //   "fieldKey": "job_id",
+      //   "value": 1234
+      // }, 
+      // {
+      //   "fieldKey": "job_date",
+      //   "value": "2015-02-20T08:00:00Z"
+      // }, 
+      // {
+      //   "fieldKey": "single_list_state",
+      //   "value": "FL"
+      // }, 
+      // {
+      //   "fieldKey": "multi_list_jobtype",
+      //   "value": ["Trenching", "Excavation"]
+      // }, 
+      // {
+      //   "fieldKey": "subform_4",
+      //   "value": [
+      //     [{
+      //       "fieldKey": "alpha_1",
+      //       "value": "Text"
+      //     }, {
+      //       "fieldKey": "numeric_2",
+      //       "value": 12345
+      //     }]
+      //   ]
+      // },
+  ]
+}
+
+
+
+
+    var ffkey = "151844_fdef3e0b14ae49c785d01368c2fb10c5.csv"
+    var xxx = ""
+    await fetch(`https://api.fastfieldforms.com/services/v3/dispatch`, {
+            method: 'POST',
+            headers: {
+                'X-Gatekeeper-SessionToken': '4b3d46cac647457d830f696b3b68a948',
+                'Cache-Control': 'no-cache',
+                'FastField-API-Key': '1a9ea71e79ab459d825f562f8aa4188d'},
+                body: JSON.stringify(data)
+        })
+        .then(response => {
+            // console.log(response.status);
+            // console.log(response.text());
+            return response.json()
+        })
+        .then(data => {
+        // xxx = data.downloadUrl;
+            console.log(data);
+        })
+        .catch(err => console.error(err));
+}
+
+//dipatchForm()
+
 
 // fetch(`https://api.fastfieldforms.com/services/v3/authenticate`, {
 //   credentials: "include",
@@ -36,26 +124,26 @@
 // console.log("Hello World");
 
 
-var ffkey = "151844_fdef3e0b14ae49c785d01368c2fb10c5.csv"
-var xxx = ""
-await fetch(`https://api.fastfieldforms.com/services/v3/media/download?key=${ffkey}`, {
-        method: 'GET',
-        // Request headers
-        headers: {
-            'X-Gatekeeper-SessionToken': 'eec597d415e44057a1a799effa8066ae',
-            'Cache-Control': 'no-cache',
-            'FastField-API-Key': '1a9ea71e79ab459d825f562f8aa4188d'}
-    })
-    .then(response => {
-        // console.log(response.status);
-        // console.log(response.text());
-        return response.json()
-    })
-    .then(data => {
-      xxx = data.downloadUrl;
-        console.log(data);
-    })
-    .catch(err => console.error(err));
+// var ffkey = "151844_fdef3e0b14ae49c785d01368c2fb10c5.csv"
+// var xxx = ""
+// await fetch(`https://api.fastfieldforms.com/services/v3/media/download?key=${ffkey}`, {
+//         method: 'GET',
+//         // Request headers
+//         headers: {
+//             'X-Gatekeeper-SessionToken': '4b3d46cac647457d830f696b3b68a948',
+//             'Cache-Control': 'no-cache',
+//             'FastField-API-Key': '1a9ea71e79ab459d825f562f8aa4188d'}
+//     })
+//     .then(response => {
+//         // console.log(response.status);
+//         // console.log(response.text());
+//         return response.json()
+//     })
+//     .then(data => {
+//       // xxx = data.downloadUrl;
+//         console.log(data);
+//     })
+//     .catch(err => console.error(err));
 
 
 // const textBlob = new Blob([
